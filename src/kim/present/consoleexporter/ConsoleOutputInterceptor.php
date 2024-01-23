63099d1f3fa9f5c7aa9a5d95af87fe479c4b1fcc
PresentKim <debe3721@gmail.com>
2024-01-24 03:05:55 +0900

    :recycle: Separate the functions of the main class into different classes
    - ConsoleOutputInterceptor: Performs all functions of intercepting console output
    
    - ConsoleOutputExporter: Performs all functions of exporting console output

---
+++ src/kim/present/consoleexporter/ConsoleOutputInterceptor.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection PhpUnused
+ */
+
+declare(strict_types=1);
+
+namespace kim\present\consoleexporter;
+
+use pocketmine\console\ConsoleCommandSender;
+use pocketmine\event\HandlerListManager;
+use pocketmine\event\Listener;
+use pocketmine\event\server\CommandEvent;
+use pocketmine\plugin\PluginOwnedTrait;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\scheduler\TaskHandler;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+final class ConsoleOutputInterceptor implements Listener{
+    use PluginOwnedTrait;
+
+    private bool $enabled = false;
+    private string $buffer = "";
+    private ?TaskHandler $taskHandler = null;
+
+    public function flushBuffer() : string{
+        $buffer = $this->buffer;
+        $this->buffer = "";
+
+        return trim($buffer);
+    }
+
+    public function writeBuffer(string $buffer) : string{
+        $this->buffer .= $buffer;
+
+        return $buffer;
+    }
+
+    public function isEnabled() : bool{
+        return $this->enabled;
+    }
+
+    public function enable() : void{
+        if($this->enabled){
+            return;
+        }
+
+        // Start capturing output buffer
+        ob_start($this->writeBuffer(...));
+        Server::getInstance()->getPluginManager()->registerEvents($this, $this->owningPlugin);
+
+        // Start flushing output buffer for avoid console messages delayed
+        $this->taskHandler = $this->owningPlugin->getScheduler()->scheduleRepeatingTask(new ClosureTask(ob_flush(...)), 1);
+
+        $this->enabled = true;
+    }
+
+    /** @priority HIGHEST */
+    public function onCommandEvent(CommandEvent $event) : void{
+        if(!$this->enabled || !($event->getSender() instanceof ConsoleCommandSender)){
+            return;
+        }
+
+        $command = trim($event->getCommand());
+        if($command[0] === "#"){ // If command starts with "#", Ignore command (comment line)
+            $event->cancel();
+        }else{ // Else, Apply italic effect to make a distinction from comments
+            $this->writeBuffer(TextFormat::ITALIC);
+        }
+        $this->writeBuffer(TextFormat::GRAY . $command . TextFormat::RESET . PHP_EOL);
+    }
+
+    public function disable() : void{
+        if(!$this->enabled){
+            return;
+        }
+
+        // Stop capturing output buffer
+        ob_end_flush();
+        HandlerListManager::global()->unregisterAll($this);
+
+        // Stop flushing output buffer
+        $this->taskHandler->cancel();
+        $this->taskHandler = null;
+
+        $this->enabled = false;
+    }
+}
--- src/kim/present/consoleexporter/Main.php
+++ src/kim/present/consoleexporter/Main.php
@@ -27,153 +27,42 @@ declare(strict_types=1);
 
 namespace kim\present\consoleexporter;
 
-use kim\present\consoleexporter\listener\ConsoleCommandListener;
-use kim\present\consoleexporter\task\FlushingBufferTask;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
-use pocketmine\scheduler\TaskHandler;
-use pocketmine\utils\Terminal;
 use pocketmine\utils\TextFormat;
 
 final class Main extends PluginBase{
-    private const HTML_EOL = "<br/>";
-    private const HTML_SPACE = "&nbsp;";
-    private const HTML_TAB = self::HTML_SPACE . self::HTML_SPACE . self::HTML_SPACE . self::HTML_SPACE;
-
-    private string $template = "";
-
-    private bool $recording = false;
-    private ?TaskHandler $taskHandler;
-    private string $buffer = "";
+    private ConsoleOutputInterceptor $interceptor;
+    private ConsoleOutputExporter $exporter;
 
     protected function onLoad() : void{
-        $template = file_get_contents($this->getResourcePath("template.html"));
-        $templateScript = file_get_contents($this->getResourcePath("template.js"));
-        $templateStyle = file_get_contents($this->getResourcePath("template.css"));
-        $templateControls = file_get_contents($this->getResourcePath("template.svg"));
-
-        $this->template = str_replace(
-            ["<!--[SCRIPT]-->", "<!--[STYLE]-->", "<!--[CONTROLS]-->"],
-            array_map(static fn(string $src) : string => preg_replace("/\s+\r*\n*\s*/", " ", $src),
-                [self::wrapTag("script", $templateScript), self::wrapTag("style", $templateStyle), $templateControls]
-            ),
-            $template
+        $this->interceptor = new ConsoleOutputInterceptor($this);
+        $this->exporter = new ConsoleOutputExporter(
+            file_get_contents($this->getResourcePath("template.html")),
+            file_get_contents($this->getResourcePath("template.js")),
+            file_get_contents($this->getResourcePath("template.css")),
+            file_get_contents($this->getResourcePath("template.svg"))
         );
     }
 
-    protected function onEnable() : void{
-        $this->getServer()->getPluginManager()->registerEvents(new ConsoleCommandListener($this), $this);
-    }
-
     protected function onDisable() : void{
-        $exportsPath = $this->stopRecoding();
-        if($exportsPath !== null){
-            $this->getLogger()->info(TextFormat::GREEN . "Console recoding stoped. Exports to " . TextFormat::DARK_GREEN . $exportsPath);
-        }
+        $this->interceptor->disable();
+        $this->interceptor->flushBuffer();
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if($this->recording){
-            $sender->sendMessage(TextFormat::GREEN . "Console recoding stoped. Exports to " . TextFormat::DARK_GREEN . $this->stopRecoding());
+        if($this->interceptor->isEnabled()){
+            $this->interceptor->disable();
+
+            $path = $this->getDataFolder() . "console-exporter-" . time() . ".html";
+            $buffer = $this->interceptor->flushBuffer();
+            $this->exporter->export($path, $buffer);
+            $sender->sendMessage(TextFormat::GREEN . "Console recoding stopped. File exported to " . TextFormat::DARK_GREEN . $path);
         }else{
+            $this->interceptor->enable();
             $sender->sendMessage(TextFormat::GREEN . "Console recoding started");
-            $this->startRecoding();
         }
         return true;
-    }
-
-    public function isRecording() : bool{
-        return $this->recording;
-    }
-
-    public function writeBuffer(string $buffer) : string{
-        if($this->recording){
-            $this->buffer .= $buffer;
-        }
-
-        return $buffer;
-    }
-
-    private function startRecoding() : void{
-        $this->recording = true;
-        $this->buffer = "";
-        ob_start([$this, "writeBuffer"]);
-        $this->taskHandler = $this->getScheduler()->scheduleRepeatingTask(new FlushingBufferTask($this), 1);
-    }
-
-    private function stopRecoding() : ?string{
-        if(!$this->recording){
-            return null;
-        }
-        ob_end_flush();
-        $this->recording = false;
-        $this->taskHandler->cancel();
-        $this->taskHandler = null;
-
-        $buffer = rtrim(str_replace("\r\n", PHP_EOL, $this->buffer));
-        if(str_contains($buffer, PHP_EOL)){
-            $buffer = substr($buffer, 0, (strlen($buffer)) - (strlen(strrchr($buffer, PHP_EOL))));
-        }else{
-            $buffer = "";
-        }
-        $this->buffer = $buffer;
-
-        $replacements = [
-            "\r" => "",
-            "\n" => self::HTML_EOL,
-            " " => self::HTML_SPACE,
-            Terminal::$FORMAT_RESET => TextFormat::RESET,
-            Terminal::$FORMAT_BOLD => TextFormat::BOLD,
-            Terminal::$FORMAT_OBFUSCATED => TextFormat::OBFUSCATED,
-            Terminal::$FORMAT_ITALIC => TextFormat::ITALIC,
-            Terminal::$FORMAT_UNDERLINE => TextFormat::UNDERLINE,
-            Terminal::$FORMAT_STRIKETHROUGH => TextFormat::STRIKETHROUGH,
-            Terminal::$COLOR_BLACK => TextFormat::BLACK,
-            Terminal::$COLOR_DARK_BLUE => TextFormat::DARK_BLUE,
-            Terminal::$COLOR_DARK_GREEN => TextFormat::DARK_GREEN,
-            Terminal::$COLOR_DARK_AQUA => TextFormat::DARK_AQUA,
-            Terminal::$COLOR_DARK_RED => TextFormat::DARK_RED,
-            Terminal::$COLOR_PURPLE => TextFormat::DARK_PURPLE,
-            Terminal::$COLOR_GOLD => TextFormat::GOLD,
-            Terminal::$COLOR_GRAY => TextFormat::GRAY,
-            Terminal::$COLOR_DARK_GRAY => TextFormat::DARK_GRAY,
-            Terminal::$COLOR_BLUE => TextFormat::BLUE,
-            Terminal::$COLOR_GREEN => TextFormat::GREEN,
-            Terminal::$COLOR_AQUA => TextFormat::AQUA,
-            Terminal::$COLOR_RED => TextFormat::RED,
-            Terminal::$COLOR_LIGHT_PURPLE => TextFormat::LIGHT_PURPLE,
-            Terminal::$COLOR_YELLOW => TextFormat::YELLOW,
-            Terminal::$COLOR_WHITE => TextFormat::WHITE,
-            Terminal::$COLOR_MINECOIN_GOLD => TextFormat::MINECOIN_GOLD,
-        ];
-        $replacedLines = explode(self::HTML_EOL, str_replace(array_keys($replacements), array_values($replacements), htmlspecialchars($buffer)));
-        $wrappedLines = array_map(static fn(string $line) : string => wordwrap($line, 180, self::HTML_EOL . self::HTML_TAB), $replacedLines);
-        $htmlContents = "";
-        $classes = [];
-        foreach(TextFormat::tokenize(implode(self::HTML_EOL, $wrappedLines)) as $token){
-            if($token === TextFormat::RESET){
-                $classes = [];
-            }elseif(preg_match("/" . TextFormat::ESCAPE . "([0-9a-g])/u", $token, $matches) > 0){
-                $classes[0] = "t$matches[1]";
-            }elseif(preg_match("/" . TextFormat::ESCAPE . "([k-o])/u", $token, $matches) > 0){
-                $classes[ord($matches[1])] = "t$matches[1]";
-            }elseif(!empty($classes)){
-                $htmlContents .= self::wrapTag("span", $token, implode(" ", $classes));
-            }else{
-                $htmlContents .= $token;
-            }
-        }
-
-        $filename = $this->getDataFolder() . "console-exporter-" . time() . ".html";
-        file_put_contents($filename, str_replace("<!--[CONTENTS]-->", $htmlContents, $this->template));
-        return $filename;
-    }
-
-    private static function wrapTag(string $tag, string $innerHTML, string $classList = "") : string{
-        if($classList === ""){
-            return "<$tag>$innerHTML</$tag>";
-        }
-        return "<$tag class=\"$classList\">$innerHTML</$tag>";
     }
 }
